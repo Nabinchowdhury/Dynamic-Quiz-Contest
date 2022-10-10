6cacb51fcebb7663008e9724b0ff1fdbc9cdc977
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-10-11 01:17:09 +0600

    home created

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ data }) => {
+    const { id, logo, name, total } = data
     return (
-        <div>
-
+        <div className="card card-compact w-auto bg-base-100 shadow-xl mx-5 ">
+            <figure><img src={logo} alt="Shoes" className=' bg-zinc-300' /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+                <h3 className='text-base text-left font-bold'>Number of Quiz:  {total}</h3>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Start Contest</button>
+                </div>
+            </div>
         </div>
+
     );
 };
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,17 +1,19 @@
 import React, { useContext } from 'react';
+import Card from '../Card/Card';
 import { apiContext } from '../Layout/Main';
 
 const Home = () => {
     const datas = useContext(apiContext)
-    console.log(datas)
     return (
-        <div>
-            {
-                datas.map(data => {
 
-                })
-            }
+        <div className='mt-24'>
+            <h1 className='text-4xl mx-24 font-bold text-rose-400 underline'>Participate our contests to test your knowledge</h1>
+            <div className='grid grid-cols-1 gap-4 m-24 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                    datas.map(data => <Card data={data}></Card>)
+                }
 
+            </div>
         </div>
     );
 };
