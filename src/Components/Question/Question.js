7881f0b839991fada3d80ac266a0298eb187802d
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-10-11 04:21:16 +0600

    eye toggle done

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Option from '../Option/Option';
+import { BeakerIcon, EyeIcon } from '@heroicons/react/24/solid'
 
 const Question = (quiz) => {
     const { id, question, correctAnswer, options } = quiz.question
     const count = quiz.count
     // console.log(options)
+    const [show, setShow] = useState(false)
     return (
         <div className='w-3/4 mx-auto rounded-lg bg-slate-50  mt-5 py-5'>
-            <h4 className='text-2xl font-600'>Quiz {count}: {question}</h4>
+            <div className='flex justify-evenly items-center'>
+                <h4 className='text-xl font-600 w-3/4'>Quiz {count}: {question}</h4>
+                <EyeIcon className="h-6 w-6 text-blue-400 hover:text-blue-700 cursor-pointer" onClick={() => setShow(!show)}></EyeIcon>
+            </div>
+            <h4 className={`text-xl font-600 w-2/4 bg-green-200 mx-auto my-4 rounded-lg p-4 ${show ? "block" : "hidden"}`}>Correct Answer: {correctAnswer}</h4>
             <div className='grid grid-cols-1 gap-4 md:grid-cols-2 mt-5'>
                 {
                     options.map((option, idx) => <Option key={idx} option={option} count={count}></Option>)
